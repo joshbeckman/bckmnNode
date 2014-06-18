8b126e5c2acf26939c45db5a49894ea8e9f7adab
j-beckman <beckman.joshua@gmail.com>
2014-06-18 00:17:03 -0500

    Cleaning

--- routes/backEnd.js
+++ routes/backEnd.js
@@ -23,18 +23,16 @@ module.exports = function (app, io, ensureAuth) {
 
   app.get('/w', ensureAuth, function(req,res){
     Post.findOne({_id: req.query.edit}).lean().exec(function(err,post){
-      Post.getUnpublishedPosts(function(err, unpublishedPosts){
-        res.render('write', {
-          title: 'Write',
-          post: post,
-          marked: marked,
-          unpublishedPosts: unpublishedPosts,
-          noTracking: true,
-          socket: true,
-          message: req.flash('message'),
-          error: req.flash('error'),
-          req: req
-        });
+      res.render('write', {
+        title: 'Write',
+        post: post,
+        marked: marked,
+        unpublishedPosts: unpublishedPosts,
+        noTracking: true,
+        socket: true,
+        message: req.flash('message'),
+        error: req.flash('error'),
+        req: req
       });
     });
   });
