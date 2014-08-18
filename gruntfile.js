69a60d14e1c381bda21771fbb19305736d1be940
j-beckman <beckman.joshua@gmail.com>
2014-08-18 11:08:42 -0500

    removing social, cleaning up package

--- gruntfile.js
+++ gruntfile.js
@@ -45,10 +45,8 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-csslint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('test', ['jshint']);
   grunt.registerTask('crush', ['uglify', 'cssmin']);
