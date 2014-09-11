71c1b2729343e0efed2c7611835e984e57c4bde1
j-beckman <beckman.joshua@gmail.com>
2014-09-10 23:38:26 -0500

    making more things work

--- routes/frontEnd.js
+++ routes/frontEnd.js
@@ -37,15 +37,15 @@ module.exports = function (app, io, ensureAuth) {
     }
   });
   app.get('/test-moi', function(req,res){
-    // blocks['words'](req, res, 'words', 'words');
-    res.render('you', { title: 'you',
-                            description: 'config.members[subnet].description',
-                            req: req,
-                            subnet: 'about',
-                            query: req.query,
-                            stripeKey: api_public,
-                            message: req.flash('message'), 
-                            error: req.flash('error') });
+    blocks['words'](req, res, 'words', 'words');
+    // res.render('you', { title: 'you',
+    //                         description: 'config.members[subnet].description',
+    //                         req: req,
+    //                         subnet: 'about',
+    //                         query: req.query,
+    //                         stripeKey: api_public,
+    //                         message: req.flash('message'), 
+    //                         error: req.flash('error') });
   });
   app.get('/post/:slug', function(req,res){
     checkSubdomain(req, res, 'words', function(){
